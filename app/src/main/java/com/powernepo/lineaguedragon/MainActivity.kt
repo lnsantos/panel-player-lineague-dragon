768e83bfd64aaa2fc9be542018b15c36f7b8cea2
Lucas Solvian <lucas.santos@solvian.com>
2020-06-05 09:25:09 -0300

    starting repository of team lineague dragon...

---
+++ app/src/main/java/com/powernepo/lineaguedragon/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.powernepo.lineaguedragon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
